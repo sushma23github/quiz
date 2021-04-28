4da2dd008f1331e4f47ba5c1fe7128d219206fc5
Sushma palli <69786123+sushma23github@users.noreply.github.com>
2021-04-28 10:18:15 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Quiz from "./Quiz";
+import Question from "./Question";
+ function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign:"center"}}> Quiz app </h1>
+      <Quiz />
+    </div>
+  );
+}
+export default App
